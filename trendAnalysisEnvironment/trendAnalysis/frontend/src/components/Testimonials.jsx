8931d6ffe79daa7cb5f4cb802f5b9245dce02ef7
Dineshsut07 <120447913+Dineshsut07@users.noreply.github.com>
2024-07-23 21:14:51 +0530

    Update Testimonials.jsx    --

--- trendAnalysisEnvironment/trendAnalysis/frontend/src/components/Testimonials.jsx
+++ trendAnalysisEnvironment/trendAnalysis/frontend/src/components/Testimonials.jsx
@@ -1,21 +1,32 @@
-import { feedback } from "../constants";
+// import { feedback } from "../constants";
+import {a} from '../assets'
 import styles from "../style";
 import FeedbackCard from "./FeedbackCard";
-
+export const feedback = [
+  {
+    
+    title: "August 2024",
+    img: a,
+  },
+  {
+    
+    title: "April 2024",
+    img: a,
+  },
+  {
+  
+    title: "March 2024",
+    img:a,
+  },
+];
 const Testimonials = () => (
   <section id="clients" className={`${styles.paddingY} ${styles.flexCenter} flex-col relative `}>
     <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient bottom-40" />
 
     <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
-      <h2 className={styles.heading2}>
-        What People are <br className="hidden sm:block" /> saying about us
-      </h2>
-      <div className="w-full mt-6 md:mt-0">
-        <p className={`${styles.paragraph} text-left max-w-[450px]`}>
-          Everything you need to accept card payments and grow your business
-          anywhere on the planet.
-        </p>
-      </div>                        
+      <h2 className=  {`${styles.heading2} text-center`}>
+        Recent Trends <br className="hidden sm:block" /> 
+      </h2>                      
     </div>
 
     <div className="flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]">
