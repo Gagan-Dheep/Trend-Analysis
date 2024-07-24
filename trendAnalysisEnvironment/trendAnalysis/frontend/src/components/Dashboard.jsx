b72ba5481c9fb3904225bf431037ea4662d5f483
Dineshsut07 <120447913+Dineshsut07@users.noreply.github.com>
2024-07-24 21:42:43 +0530

    Update Dashboard.jsx

--- trendAnalysisEnvironment/trendAnalysis/frontend/src/components/Dashboard.jsx
+++ trendAnalysisEnvironment/trendAnalysis/frontend/src/components/Dashboard.jsx
@@ -1,19 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import styles from '../style';
 import Testimonials from './Testimonials';
+import { a } from '../assets';
 
 const Dashboard = () => {
   const [selectedMonth, setSelectedMonth] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const [predictionData, setPredictionData] = useState(null);
   const [datas, setdatas] = useState('');
+  const [timestamp, setTimestamp] = useState(Date.now());
+
   const handleInputChange = (event) => {
     setSelectedMonth(event.target.value);
   };
 
   const handlePredict = async () => {
     if (!selectedMonth) return; // Do not fetch if selectedMonth is empty
-    
+
     setIsLoading(true);
     setPredictionData(null);
 
@@ -29,6 +32,7 @@ const Dashboard = () => {
 
       const data = await response.json();
       setPredictionData(data);
+      setTimestamp(Date.now());
       console.log(data);
       datas(setPredictionData.bar_plot);
     } catch (error) {
@@ -37,14 +41,10 @@ const Dashboard = () => {
       setIsLoading(false);
     }
   };
-  
-  // useEffect(() => {
-  //   handlePredict(); // Initial fetch when component mounts or selectedMonth changes
-  // }, [selectedMonth]);
 
   return (
-    <div className="p-8 h-[200vh]">
-      <h1 className="mb-8 text-6xl font-bold text-center text-white">Trend Prediction Model</h1>
+    <div className="min-h-screen p-8">
+      <h1 className="mb-8 text-4xl font-bold text-center text-white md:text-6xl">Trend Prediction Model</h1>
 
       <div className="mb-6 text-center text-white">
         <label htmlFor="month" className="block mb-2 text-lg font-bold">
@@ -58,7 +58,7 @@ const Dashboard = () => {
           className="p-2 text-black border rounded-lg"
           placeholder="e.g., 8 for August 2024"
         />
-        <button onClick={handlePredict} className="p-2 ml-4 text-white bg-blue-500 rounded-lg">
+        <button onClick={handlePredict} className="p-2 mb-20 ml-4 text-white bg-blue-500 rounded-lg">
           Predict
         </button>
       </div>
@@ -66,53 +66,47 @@ const Dashboard = () => {
       {isLoading ? (
         <div className="text-xl text-center text-white">Loading...</div>
       ) : predictionData ? (
-        <div className="flex flex-wrap justify-center gap-6">
-          <div className="w-full bg-white shadow-lg md:w-1/2 lg:w-1/4 rounded-xl">
-            <div className="p-4">
-              <h2 className="mb-4 text-xl font-bold text-white">Bar Chart</h2>
-              <img
-                src={`http://127.0.0.1:8000/${predictionData.bar_plot}`}
-                alt="Bar Chart"
-                className="w-full h-auto"
-              />
-            </div>
+        <div className="space-y-6">
+          <h2 className='text-4xl font-bold text-center text-white'> The Prediction data for {selectedMonth}th Month is: </h2>
+
+          <div className="w-full p-4 mx-auto bg-white shadow-lg md:w-3/4 lg:w-1/2 rounded-xl mb-7">
+            <label className="block mb-2 text-3xl font-bold text-center text-black">Bar Chart</label>
+            <img
+              src={`http://127.0.0.1:8000/${predictionData.bar_plot}?timestamp=${timestamp}`}
+              alt="Bar Chart"
+              className="object-contain w-full h-96"
+            />
           </div>
 
-          <div className="w-full bg-white shadow-lg md:w-1/2 lg:w-1/4 rounded-xl">
-            <div className="p-4">
-              <h2 className="mb-4 text-xl font-bold">Line Chart</h2>
-              <img
-                src={`http://127.0.0.1:8000/${predictionData.line_plot}`}
-                alt="Line Chart"
-                className="w-full h-auto"
-              />
-            </div>
+          <div className="w-full p-4 mx-auto bg-white shadow-lg md:w-3/4 lg:w-1/2 rounded-xl mb-7">
+            <label className="block mb-2 text-3xl font-bold text-center text-black">Line Chart</label>
+            <img
+              src={`http://127.0.0.1:8000/${predictionData.line_plot}?timestamp=${timestamp}`}
+              alt="Line Chart"
+              className="object-contain w-full h-96"
+            />
           </div>
 
-          <div className="w-full bg-white shadow-lg md:w-1/2 lg:w-1/4 rounded-xl">
-            <div className="p-4">
-              <h2 className="mb-4 text-xl font-bold">Pie Chart</h2>
-              <img
-                src={`http://127.0.0.1:8000/${predictionData.pie_plot}`}
-                alt="Pie Chart"
-                className="w-full h-auto"
-              />
-            </div>
+          <div className="w-full p-4 mx-auto bg-white shadow-lg md:w-3/4 lg:w-1/2 rounded-xl mb-7">
+            <label className="block mb-2 text-3xl font-bold text-center text-black">Pie Chart</label>
+            <img
+              src={`http://127.0.0.1:8000/${predictionData.pie_plot}?timestamp=${timestamp}`}
+              alt="Pie Chart"
+              className="object-contain w-full h-96"
+            />
           </div>
 
-          <div className="w-full bg-white shadow-lg md:w-1/2 lg:w-1/4 rounded-xl">
-            <div className="p-4">
-              <h2 className="mb-4 text-xl font-bold">Area Chart</h2>
-              <img
-                src={`http://127.0.0.1:8000/${predictionData.rel_plot}`}
-                alt="Area Chart"
-                className="w-full h-auto"
-              />
-            </div>
+          <div className="w-full p-4 mx-auto bg-white shadow-lg md:w-3/4 lg:w-1/2 rounded-xl mb-7">
+            <label className="block mb-2 text-3xl font-bold text-center text-black">Area Chart</label>
+            <img
+              src={`http://127.0.0.1:8000/${predictionData.rel_plot}?timestamp=${timestamp}`}
+              alt="Area Chart"
+              className="object-contain w-full h-96"
+            />
           </div>
         </div>
       ) : (
-        <div className="text-xl text-center text-white mt-11 mb-20">
+        <div className="mb-20 text-xl text-center text-white mt-11">
           Please enter a month and click "Predict" to see the trend predictions.
         </div>
       )}
